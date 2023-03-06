a187f9b9fb910599cf54d75d37900ea19ce971e3
Evgeniy <onving@gmail.com>
2023-03-06 14:43:15 +0300

    Сделана на чистовую обработка арабских цифр. Нужно сделать римские

--- src/ArabicString.java
+++ src/ArabicString.java
@@ -2,22 +2,25 @@ class ArabicString {
 
     static void calculate(String rawString) throws Exception {
 
-        rawString = rawString.trim();
-        System.out.println(   rawString.matches("^[0-9\\s+-/\\*]*$"));
-       // System.out.println( rawString.matches("^[*/]*$"));
-       // input.matches("^[+-0-9]+$");
+        //если в строке есть не цифры и не +-*/ то ошибка
         if(!rawString.matches("^[0-9\\s+-/\\*]*$")){
             throw new Exception("введены некоректные символы");
         }
 
+        //разбиваем на числа по +-*/
         String[] splitArray = rawString.split("[+-/\\*]");
-        if(splitArray.length!=2) throw new Exception("введены некоректные данные");
-        int a = Integer.parseInt(splitArray[0]);
-        int b = Integer.parseInt(splitArray[1]);
-        System.out.println(a);
-        System.out.println(b);
 
-        //!!!!!!!!!!!!!!!! добавить проверку 0-10!!!!!!!!!!!!!!!!!!
+        //если получили больше 2х чисел то ошибка (формат математической операции не удовлетворяет заданию - два операнда и один оператор)
+        if(splitArray.length!=2) throw new Exception("введены некоректные данные");
+
+        //убираем проберы перед и преобразоввываем в числа
+        int a = Integer.parseInt((splitArray[0]).trim());
+        int b = Integer.parseInt((splitArray[1]).trim());
+
+        //проверяем что числа в диапазоне 1-10
+        if (a<1||a>10||b<1||b>10){
+            throw new Exception("число(а) не входят в диапазон \'1-10\'");
+        }
         
         if(rawString.contains("+")){
             System.out.println(a+b);
@@ -31,7 +34,4 @@ class ArabicString {
 
 
     }//calculate
-
-
-
 }//ArabicString
